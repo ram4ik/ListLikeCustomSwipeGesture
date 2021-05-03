a2e65db4904d9d857cc4457331027a62a43bdc48
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-05-03 14:00:11 +0300

    Initial Commit

---
+++ ListLikeCustomSwipeGesture/ListLikeCustomSwipeGestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListLikeCustomSwipeGestureApp.swift
+//  ListLikeCustomSwipeGesture
+//
+//  Created by ramil on 03.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct ListLikeCustomSwipeGestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
